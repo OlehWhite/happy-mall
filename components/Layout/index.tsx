e71704e4496c27fc4b6935d217ceac1def605959
Bilostotskyi Oleh <oleg220298d@gmail.com>
2023-09-10 10:36:15 +0300

    fix bug for vercel

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,8 +1,12 @@
-import { FC, PropsWithChildren } from "react";
+import { FC, ReactNode } from "react";
 import { Container } from "./style";
 
-const Main: FC = ({ children }: PropsWithChildren<{}>) => {
+type LayoutProps = {
+  children: ReactNode;
+};
+
+const Layout: FC<LayoutProps> = ({ children }) => {
   return <Container>{children}</Container>;
 };
 
-export default Main;
+export default Layout;
